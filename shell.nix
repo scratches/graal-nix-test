010eee7125174cdb8a1bcedbd9c797bbc2af9d2e
Dave Syer <dsyer@vmware.com>
2024-04-04 17:42:15 +0100

    Nuclear option with GRAAL_FLAGS

--- shell.nix
+++ shell.nix
@@ -5,4 +5,7 @@ mkShell {
     zlib
     zlib.static
   ];
+  shellHook = ''
+    export GRAAL_FLAGS=$(env | grep ^NIX | sed -e 's/=.*//' | tr '\n' ' ' | sed -e 's/ *$//' | sed -e 's/ / -E/g' | sed -e 's/^NIX/-ENIX/')
+  '';
 }
